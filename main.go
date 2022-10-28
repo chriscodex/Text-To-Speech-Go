0fd75dd176e619c65dc57d93c28f84dbffc49372
Christian <gonzalo.espinozacadillo@gmail.com>
2022-10-28 18:21:36 -0500

    speech object created

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	htgotts "github.com/hegedustibor/htgo-tts"
+	"github.com/hegedustibor/htgo-tts/voices"
+)
 
+func main() {
+	speech := htgotts.Speech{
+		Folder:   "audio",
+		Language: voices.English,
+	}
 }
